9280225a6b0615adc01509200913705c654f4935
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-10-16 20:45:32 -0400

    Custom hook and abort controller

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export default axios.create({
+export const apiClient = axios.create({
    baseURL: 'https://api.rawg.io/api',
    params:{
    key: '1a8e8b459d014cbcb17354c86e53b6cf' 
    }
-})
+});
