1413319563f777721ad7f4bcccd3f1da69bb16d7
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-11-04 17:43:12 -0400

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,17 @@ function App() {
                 base: `"nav" "main"`, //mobile devices
                 lg: `"nav nav" "aside main"`, //large devices wider than 1024 pixels
             }}
+            templateColumns={{
+                base: "1fr",
+                lg: "200px 1fr",
+            }}
         >
             <GridItem area="nav">
                 <NavBar />
             </GridItem>
 
             <Show above="lg">
-                <GridItem area="aside">
+                <GridItem area="aside" paddingX={5}>
                     <GenreList />
                 </GridItem>
             </Show>
